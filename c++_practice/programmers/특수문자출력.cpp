6c817360a8ced2fd70820d15575319863260aad1
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2023-06-30 17:29:46 +0900

    Create 특수문자출력.cpp

---
+++ c++_practice/programmers/특수문자출력.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+int main(void) {
+	string a = "!@#$ % ^&*(\\'\"<>?:;";//따음표 앞에 \ 적어주고 출력하고자하는 \ 앞에 \ 적을것
+	cout << a;
+}
