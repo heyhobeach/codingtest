4c5dac45bdbc706d349f0b09b3078cdde8cd92fa
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2023-11-01 14:53:56 +0900

    Update 달리기경주.cpp

--- 프로그래머스python/달리기경주.cpp
+++ 프로그래머스python/달리기경주.cpp
@@ -38,3 +38,31 @@ int main(void) {
     }
     return 0;
 }
+
+
+
+///////
+
+int main(void) {
+    
+    vector<string> callings{ "kai", "kai", "mine", "mine" };
+    vector<string> players{ "mumu", "soe", "poe", "kai", "mine" };
+    vector<string> answer;
+    int pos = 0;
+    string temp = "";
+    unordered_map<string, int> m_players;
+
+    for (int i = 0; i < players.size(); i++) {
+        m_players[players[i]] = i;
+    }
+
+    for (auto i : callings) {
+        pos = m_players[i];//위치 찾기
+        cout << players[pos-1] << ", " << players[pos ] << endl;
+        swap(players[pos], players[pos - 1]);
+        cout << players[pos-1] << ", " << players[pos ]<<endl;
+        m_players[players[pos]] = pos;//위치 변경
+        m_players[players[pos - 1]] = pos - 1;//위치 변경
+    }
+    return 0;
+}
