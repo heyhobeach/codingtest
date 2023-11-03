56bd9dbf460710005e87c40b3ce6c0e1f1945999
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2023-11-03 16:47:44 +0900

    Update 최소직사각형.cpp

--- c++_practice/programmers/최소직사각형.cpp
+++ c++_practice/programmers/최소직사각형.cpp
@@ -44,3 +44,39 @@ int main(void) {//최소직사각형 완전탐색
     cout << answer;
     return 0;
 }
+
+//////////////////////////////
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int findSmallestWalletSize(vector<vector<int>>& sizes, int index, int width, int height) {
+    if (index == sizes.size()) {
+        return width * height;
+    }
+
+    int w = sizes[index][0];
+    int h = sizes[index][1];
+
+    // 명함을 가로로 눕혔을 때와 세로로 눕혔을 때 두 가지 경우를 모두 시도
+    cout << "width :" << width << " w :" << w <<"\theight :"<<height<<"h :"<<h<< endl;
+    int option1 = findSmallestWalletSize(sizes, index + 1, max(width, w), max(height, h));
+    cout << "width :" << width << " w :" << w << "\theight :" << height << "h :" << h << endl;
+    int option2 = findSmallestWalletSize(sizes, index + 1, max(width, h), max(height, w));
+
+    return min(option1, option2);
+}
+
+int solution(vector<vector<int>>& sizes) {
+    return findSmallestWalletSize(sizes, 0, 0, 0);
+}
+
+int main() {
+    vector<vector<int>> sizes = { {60, 50}, {30, 70}, {60, 30}, {80, 40} };
+    int result = solution(sizes);
+    cout << result << endl; // 출력값: 4000
+    return 0;
+}
