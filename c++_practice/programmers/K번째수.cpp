474bbf61943454326118811539b2d10a0760f8cf
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2023-11-15 14:15:00 +0900

    Create K번째수.cpp

---
+++ c++_practice/programmers/K번째수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for(auto i:commands){
+        vector<int> temp;
+        for(int j=i[0]-1;j<i[1];j++){
+            temp.push_back(array[j]);
+        }
+        sort(temp.begin(),temp.end());
+        answer.push_back(temp[i[2]-1]);
+    }
+    return answer;
+}
